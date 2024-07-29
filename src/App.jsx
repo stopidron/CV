b7233a4fc39ba9a64fa2c7ad151667586c3b84ad
Erona <eronagash@hotmail.com>
2024-07-29 14:07:07 +0200

    Submit name fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function Button () {
-  return <button>SUBMIT</button>
+  return <button type="submit">SUBMIT</button>
 }
-
 
 function App() {
   const [name, setName] = useState('');
-  const[person, setPerson] = useState({name: "name", surname:"surname"})
+  const[person, setPerson] = useState({personName: ""})
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPerson({personName: name})
+    console.log("name is now", name)
+  }
 
   return (
     <>
-    <input 
+    <form onSubmit = {handleSubmit}>
+    <input id="name"
     type="text"
     value = {name}
+    onChange = {e => setName(e.target.value)}
+    placeholder='enter your name'
     />
     <Button />
+    </form>
     </>
   );
-
-
 }
 
 export default App
