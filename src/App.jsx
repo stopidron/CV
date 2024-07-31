39ad5e7ef8866e109db704512ebce5b64e795eef
Erona <eronagash@hotmail.com>
2024-07-31 11:06:47 +0200

    Component in a different file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Input from './Input';
+
 
 function App() {
 
@@ -23,37 +25,18 @@ function App() {
     console.log("Data", person)
   }
 
-  const formFields = [
+  const general = [
     { id: 'name', placeholder: 'Enter your name', unique: 'personName'},
     { id: 'surname', placeholder: 'Enter your surname', unique: 'personSurname'},
     { id: 'email', placeholder: 'Enter your email', unique: 'personEmail'},
     {id: 'phone', placeholder: 'Enter your phone number', unique: 'personPhone'}
   ];
 
-  function Input(fields, change, thing){
-    console.log("formFields", formFields);  
-    return (
-      <>
-      {formFields.map(field => (
-            <input
-              key={field.id}
-              id={field.id}
-              type="text"
-              name={field.unique}
-              value={person[field.unique]}
-              onChange={handleChange}
-              placeholder={field.placeholder}
-            />
-          ))}
-      </>
-    );
-  }
-
   return (
     <form onSubmit={handleSubmit}>
       <section>
       <Input 
-      fields = {formFields}
+      info = {general}
       change = {handleChange}
       thing = {person}
       />
